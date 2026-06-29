d82be68b4b3d7da983b4d1736f3e3dba3c2880ca
whiletrue111 <whiletrue111@gmail.com>
2026-06-29 12:17:12 +0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import datetime
+import socket
+from time import timezone
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/api/v1/info")
+def root() -> dict[str,str]:
+    return {
+        "message": "FastAPI is running",
+        "datetime": datetime.now(timezone.utc).isoformat(),
+        "hostname": socket.gethostname(),
+    }
+
+@app.get("/api/v1/health")
+def health() -> dict[str,str]:
+    return {"status": "ok"}
+
+
+
